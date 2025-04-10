3d8f7d0c851f3d36062c6416c7c819d8a88d3db4
pandharkardeep <123973969+pandharkardeep@users.noreply.github.com>
2025-04-10 14:55:25 +0530

    Update config.py

--- flask-trading-app/config.py
+++ flask-trading-app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://pandharkardeep35:7762Q0QmsBVhYqLF@deep.pfmz7xz.mongodb.net/Users'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')  or "8d3b7ee5ea0a46fab86f1f0043c8e132"# For trading API
-    API_KEY = os.environ.get('API_KEY') or "74b10f24c9msh9fd947632f86905p187aebjsne3e91483531a"  # For news API
+    MONGO_URI = os.environ.get('MONGO_URI') 
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY') 
+    API_KEY = os.environ.get('API_KEY')
     DEBUG = os.environ.get('DEBUG', 'False') == 'True'
